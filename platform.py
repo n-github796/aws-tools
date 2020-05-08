ced575b95f752d5adb2d15032e3b54b81c307475
unknown <4t1OO94SsO@tutanota.com>
2020-05-08 17:42:22 -0400

    Added synopsis & usage

---
+++ platform.py
@@ -0,0 +1,3 @@
+#Synopsis: script detects platform & outputs detailed information regarding said platform.
+#Usage: python platform.py
+#Usage: import to use with other source, via 'import platform'
